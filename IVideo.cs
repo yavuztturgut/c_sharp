369370e3ca6ff40554f14e45e399da1b5a166d8a
Yavuz Turgut <163773115+yavuztturgut@users.noreply.github.com>
2024-05-24 13:43:52 +0300

    Update IVideo.cs

--- IVideo.cs
+++ IVideo.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Data;
+using System.Linq.Expressions;
 
 interface IVideo
 {
@@ -44,51 +46,126 @@ public class Program
 {
     static void AddMovie()
     {
-        Console.WriteLine("Enter movie name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("Enter the rating: ");
-        int rating = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the director name: ");
-        string director = Console.ReadLine();
+        try
+        {
+            Console.WriteLine("Enter movie name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the rating: ");
+            int rating = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the director name: ");
+            string director = Console.ReadLine();
 
-        Movie movie = new Movie(name, rating, director);
-        videos.Add(movie);
-        Console.WriteLine("Movie added!");
+            Movie movie = new Movie(name, rating, director);
+            videos.Add(movie);
+            Console.WriteLine("Movie added!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input!");
+        }
     }
     static void AddSeries()
     {
-        Console.WriteLine("Enter series name: ");
-        string name = Console.ReadLine();
-        Console.WriteLine("Enter the rating: ");
-        int rating = int.Parse(Console.ReadLine());
-        Console.WriteLine("Enter the director name: ");
-        string director = Console.ReadLine();
+        try
+        {
+            Console.WriteLine("Enter series name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the rating: ");
+            int rating = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the director name: ");
+            string director = Console.ReadLine();
 
-        Series series = new Series(name, rating, director);
-        videos.Add(series);
-        Console.WriteLine("Movie added!");
+            Series series = new Series(name, rating, director);
+            videos.Add(series);
+            Console.WriteLine("Movie added!");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input!");
+        }
     }
     static void Display()
     {
-        foreach (var video in videos)
+        try
         {
-            if (video is Movie)
+            foreach (var video in videos)
             {
-                ((Movie)video).DisplayInfo();
+                if (video is Movie)
+                {
+                    ((Movie)video).DisplayInfo();
+                }
+                else if (video is Series)
+                {
+                    ((Series)video).DisplayInfo();
+                }
             }
-            else if (video is Series)
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input!");
+        }
+    }
+    static void Delete()
+    {
+        try
+        {
+            Console.WriteLine("Enter the name of the video you want to delete: ");
+            string deleter = Console.ReadLine();
+            IVideo videotodelete = null;
+            foreach (var video in videos)
             {
-                ((Series)video).DisplayInfo();
+                if (video.name.ToLower() == deleter.ToLower())
+                {
+                    videotodelete = video;
+                    videos.Remove(videotodelete);
+                    break;
+                }
+
             }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input!");
         }
     }
     static List<IVideo> videos = new List<IVideo>();
     public static void Main(string[] args)
     {
-        
-        AddMovie();
-        AddSeries();
-        Display();
+
+        while (true)
+        {
+            try
+            {
+                Console.WriteLine("1 FOR ADDING\n2 FOR DISPLAYING\n3 FOR DELETING ");
+                int menu = int.Parse(Console.ReadLine());
+                if (menu == 1)
+                {
+                    Console.WriteLine("1 FOR MOVIES\n2 FOR SERIES");
+                    menu = int.Parse(Console.ReadLine());
+                    if (menu == 1)
+                    {
+                        AddMovie();
+                    }
+                    else if (menu == 2)
+                    {
+                        AddSeries();
+                    }
+                }
+                else if (menu == 2)
+                {
+                    Display();
+                }
+                else if (menu == 3)
+                {
+                    Delete();
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input!");
+            }
+
+        }
+
     }
-    
 }
